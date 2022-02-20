54db53c381208cd0d58e360ba13f264e07ce6194
Daniil <daniil.razgulin@mail.ru>
2022-02-21 00:24:37 +0300

    cretereq done

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,31 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options = {}) => {
-
+  //console.log(options);
+  const xhr = new XMLHttpRequest();
+  if ((options.data) && (options.data.addUrl)) {
+      options.url += options.data.addUrl;
+  }
+  xhr.responseType = 'json';
+  try {
+      xhr.open(options.method, options.url);
+      if (options.data && options.data.id) {
+          const form = new FormData();
+          form.append('id', options.data.id);
+          xhr.send(form);
+      } else {
+          xhr.send(options.data);
+      } 
+  } catch(e) {
+      options.callback(new Error(e.message), null);
+  }
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+          ///console.log(xhr.response);
+          if((xhr.response) && (xhr.response.success)) {
+              options.callback(null, xhr.response);
+          } else if (xhr.response) {
+              options.callback(xhr.response.error, null);
+          }
+      }
+  };
+  //console.log(xhr);
 };
